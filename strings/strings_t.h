976f3fccf927d48b6ff5f319eb6f13b6b5569a34
s3b4stian <sebastian.rapetti@tim.it>
2021-06-04 19:58:56 +0200

    strings updated

--- strings/strings_t.h
+++ strings/strings_t.h
@@ -1,13 +1,6 @@
 #ifndef _STRINGS_T_H
 #define _STRINGS_T_H 1
 
-//#define CONCAT(a, b) CONCAT_INNER(a, b)
-//#define CONCAT_INNER(a, b) a ## b
-
-//#define UNIQUE_NAME(base) CONCAT(base, __COUNTER__)
-
-#define PASTE_HELPER(a,b) a ## b
-#define PASTE(a,b) PASTE_HELPER(a,b)
 
 //copy S2 to S1
 #define STRING_COPY(S1, S2)                    \
@@ -15,14 +8,15 @@
     string_t S1 = string_copy(S1##_buffer, S2) \
 
 //concat S1 to S2 and put result in S2
-#define STRING_CONCAT(S3, S1, S2)                             \
+#define STRING_CONCAT(S3, S1, S2)                    \
     char S3##_buffer[S1.len + S2.len_null];          \
     string_t S3 = string_concat(S3##_buffer, S1, S2) \
 
 //trim left string S1 considering chars in S2 and put result in S3
-#define STRING_TRIM_LEFT(S3, S1, S2)                              \
+#define STRING_TRIM_LEFT(S3, S1, S2)                     \
     char S3##_buffer1[S1.len_null];                      \
     string_t S3 = string_trim_left(S3##_buffer1, S1, S2) \
+
 
 //string struct
 typedef struct {
--- strings/test.c
+++ strings/test.c
@@ -6,30 +6,30 @@
 int main(void)
 {
     string_t s1 = string_init("Hello ");
-    printf("string_init s1[%d] [%d] [%s]\n", s1.len, s1.len_null, s1.string);
+    printf("string_init s1[%d][%d] [%s]\n", s1.len, s1.len_null, s1.string);
 
     string_t s2 = string_init("Word");
-    printf("string_init s2[%d] [%d] [%s]\n", s2.len, s2.len_null, s2.string);
+    printf("string_init s2[%d][%d] [%s]\n", s2.len, s2.len_null, s2.string);
 
-    //char buffer[s1.len + s2.len_null];
-    //string_t s3 = string_concat(buffer, s1, s2);
+    //macro expand to
+    //char s3_buffer[s1.len + s2.len_null];
+    //string_t s3 = string_concat(s3_buffer, s1, s2);
     STRING_CONCAT(s3, s1, s2);
-    printf("string_concat s3[%d] [%d] [%s]\n", s3.len, s3.len_null, s3.string);
+    printf("string_concat s3[%d][%d] [%s]\n", s3.len, s3.len_null, s3.string);
     
-
-    //char buffer1[s3.len_null];
-    //string_t s4 = string_copy(buffer1, s3);
+    //macro expand to
+    //char s4_buffer[s3.len_null];
+    //string_t s4 = string_copy(s4_buffer, s3);
     STRING_COPY(s4, s3);
-    printf("string_copy [%s] [%s]\n", s3.string, s4.string);
-
+    printf("string_copy [%s][%s]\n", s3.string, s4.string);
 
     string_t s5 = string_init("  \tWord");
-
-    //char buffer1[s5.len_null];
-    //string_t s6 = string_trim_left(buffer1, s5, " ");
+    //macro expand to
+    //char s6_buffer[s5.len_null];
+    //string_t s6 = string_trim_left(s6_buffer, s5, " \t");
     STRING_TRIM_LEFT(s6, s5, " \t");
-    printf("string_trim_left s2[%d] [%d] [%s]\n", s5.len, s5.len_null, s5.string);
-    printf("string_trim_left s2[%d] [%d] [%s]\n", s6.len, s6.len_null, s6.string);
+    printf("string_trim_left s2[%d][%d] [%s]\n", s5.len, s5.len_null, s5.string);
+    printf("string_trim_left s2[%d][%d] [%s]\n", s6.len, s6.len_null, s6.string);
 
     return 0;
 }
