f860e04db1ca0766e04d64f64604d47a3a8934da
s3b4stian <sebastian.rapetti@tim.it>
2021-06-22 08:11:36 +0200

    CLP updated

--- CLP/socket.c
+++ CLP/socket.c
@@ -10,6 +10,8 @@
 
 #define h_addr h_addr_list[0] /* for backward compatibility */
 
+//check if returned pointer is null
+//print the message exploiting short circuit
 #define CALL(x) if(!(x) && fprintf(stderr, "'%s' failed (errno=%d)\n",#x,errno)) exit(1)
 
 int main (int argc, char *argv[])
@@ -19,27 +21,63 @@ int main (int argc, char *argv[])
         return 0;
     }
 
+    //struct servent {
+    //  char  *s_name;      // official service name
+    //  char **s_aliases;   // alias list
+    //  int    s_port;      // port number
+    //  char  *s_proto;     // protocol to use
+    //}
     struct servent *sent;
     CALL((sent = getservbyname("http", "tcp")) != NULL);
     int port = sent->s_port;
 
+    //struct protoent {
+    //  char  *p_name;      // official protocol name
+    //  char **p_aliases;   // alias list
+    //  int    p_proto;     // protocol number
+    //}
     struct protoent *pent;
-    //if (!pent) {exit(1);}
-    CALL ((pent = getprotobyname("tcp")) != NULL);
+    //returns a protoent structure for
+    //the entry from the database that matches the protocol name name.
+    CALL((pent = getprotobyname("tcp")) != NULL);
 
+
+    //struct hostent {
+    //  char  *h_name;      // official name of host
+    //  char **h_aliases;   // alias list
+    //  int    h_addrtype;  // host address type
+    //  int    h_length;    // length of address
+    //  char **h_addr_list; // list of addresses
+    //}
+    //#define h_addr h_addr_list[0] //for backward compatibility
     struct hostent *hent;
-    CALL ((hent = gethostbyname(argv[1])) != NULL);
-    printf ("%s -> %s\n", hent->h_name, inet_ntoa (*((struct in_addr *) hent->h_addr)));
+    //returns a structure of type hostent for the given host name.
+    CALL((hent = gethostbyname(argv[1])) != NULL);
+    printf("%s -> %s\n", hent->h_name, inet_ntoa(*((struct in_addr *) hent->h_addr)));
 
+
+    //struct sockaddr_in {
+    //  a_family_t     sin_family; // address family: AF_INET
+    //  in_port_t      sin_port;   // port in network byte order
+    //  struct in_addr sin_addr;   // internet address
+    //};
+    //Internet address
+    //struct in_addr {
+    //  uint32_t       s_addr;     // address in network byte order
+    //};
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = port;
     addr.sin_addr = *((struct in_addr *) hent->h_addr);
-    memset (addr.sin_zero, 0, 8);
+    memset(addr.sin_zero, 0, 8);
 
+    //tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
+    //udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    //raw_socket = socket(AF_INET, SOCK_RAW, protocol);
     int sock;
-    CALL ((sock = socket (AF_INET, SOCK_STREAM, pent->p_proto)) > 0);
-    CALL ((connect(sock, (struct sockaddr *) &addr, sizeof (struct sockaddr)))== 0);
+    CALL((sock = socket(AF_INET, SOCK_STREAM, pent->p_proto)) > 0);
+    
+    CALL((connect(sock, (struct sockaddr *) &addr, sizeof(struct sockaddr))) == 0);
 
     char buff[1024 + 1];
     sprintf(buff, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n",argv[1]);
