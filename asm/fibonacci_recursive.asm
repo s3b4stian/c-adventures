b6a25c1b764c3d6b1f4527305f904e7b098b227e
s3b4stian <sebastian.rapetti@tim.it>
2022-05-10 08:54:10 +0200

    fibonacci recursive updated

--- asm/fibonacci_recursive.asm
+++ asm/fibonacci_recursive.asm
@@ -20,47 +20,55 @@ main:
 
 # function fibonacci
 # recursive version
+# int fib(int n)
+# {
+#     if (n <= 1) {
+#         return n;
+#     }
+# 
+#     return fib(n - 2) + fib(n - 1);
+# }
 fibonacci:
-    subi $sp, $sp, 16
-    sw $ra, 8($sp)
-    sw $fp, 4($sp)
-    sw $s0, 0($sp)
+    subi $sp, $sp, 16           # preserve space in stack
+    sw $ra, 8($sp)              # save return address
+    sw $fp, 4($sp)              # save frame pointer
+    sw $s0, 0($sp)              
 
-    move $fp, $sp
+    move $fp, $sp               # current stack pointer as frame pointer
 
-    sw $a0, 12($fp)
-    lw  $v0, 12($fp)
+    sw $a0, 12($fp)             # save function parameter n
+    lw  $v0, 12($fp)            # load function parameter in $v0 
     nop
 
-    li $t0, 1
-    ble $v0, $t0, base_case
+    li $t0, 1                   # if n <= 1 then
+    ble $v0, $t0, base_case     # jump to base case
     nop
-    lw  $v0, 12($fp)
-    nop
-
-    sub $v0, $v0, 2
-    move $a0, $v0
-    jal fibonacci
+    lw  $v0, 12($fp)            # load function parameter n in $v0 
     nop
 
-    move $s0, $v0
-
-    lw  $v0, 12($fp)
+    sub $v0, $v0, 2             # decrement by 2 the n parameter as argument for recursive function call
+    move $a0, $v0               # argument to $a0
+    jal fibonacci               # recursive call 
     nop
 
-    sub $v0, $v0, 1
-    move $a0, $v0
-    jal fibonacci
+    move $s0, $v0               # move return function value to $s0
+
+    lw  $v0, 12($fp)            # load fuction parameter n in $v0
     nop
 
-    add $v0, $s0, $v0   
+    sub $v0, $v0, 1             # decrement by 1 the n parameter as argument for the 2nd recursive function call
+    move $a0, $v0               # argument to $a0
+    jal fibonacci               # recursive call
+    nop
+
+    add $v0, $s0, $v0           # sum to $v0 the previous return value $s0 and the current $v0
 
     base_case:
-        move $sp, $fp
-        lw $ra, 8($sp)
-        lw $fp, 4($sp)
-        lw $s0, 0($sp)
+        move $sp, $fp           # restore the stack pointer
+        lw $ra, 8($sp)          # load return address
+        lw $fp, 4($sp)          # load the frame pointer
+        lw $s0, 0($sp)          # load the return value
 
-        addi $sp, $sp, 16
-        jr $ra
+        addi $sp, $sp, 16       # free stack space
+        jr $ra                  # jump to return address
         nop
